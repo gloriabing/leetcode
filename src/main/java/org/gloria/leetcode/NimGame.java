59e39b78f879c110d8426023bc28e91c8d89b31f
Gloria <zhangbing1990@126.com>
2017-01-12 16:51:27 +0800

    Nim Game

---
+++ src/main/java/org/gloria/leetcode/NimGame.java
@@ -0,0 +1,14 @@
+package org.gloria.leetcode;
+
+/**
+ * Create on 2017/1/12 16:25.
+ *
+ * @author : gloria.
+ */
+public class NimGame {
+
+    public boolean canWinNim(int n) {
+        return n % 4 == 0 ? false : true;
+    }
+    
+}
