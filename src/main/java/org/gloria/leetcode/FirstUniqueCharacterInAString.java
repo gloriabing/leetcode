714f3c6d7e749c6e8e804828bb3a7cef19f189c2
Gloria <zhangbing1990@126.com>
2017-02-06 17:45:33 +0800

    g

--- src/main/java/org/gloria/leetcode/FirstUniqueCharacterInAString.java
+++ src/main/java/org/gloria/leetcode/FirstUniqueCharacterInAString.java
@@ -9,6 +9,19 @@ import java.util.Set;
  * Create on 2017/2/6 17:23.
  *
  * @author : gloria.
+ * 
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ *
+ * Examples:
+ *
+ * s = "leetcode"
+ * return 0.
+ *
+ * s = "loveleetcode",
+ * return 2.
+ * Note: You may assume the string contain only lowercase letters.
+ *
+ * @see : https://leetcode.com/problems/first-unique-character-in-a-string/
  */
 public class FirstUniqueCharacterInAString {
 
