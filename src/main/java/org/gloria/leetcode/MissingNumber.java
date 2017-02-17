6e2397fcb5e8549a2911d4c77e91f003e6525e07
Gloria <zhangbing1990@126.com>
2017-02-17 15:56:17 +0800

    Missing Number

---
+++ src/main/java/org/gloria/leetcode/MissingNumber.java
@@ -0,0 +1,32 @@
+package org.gloria.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Create on 2017/2/17 15:48.
+ *
+ * @author : gloria.
+ */
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length + 1; i++) {
+
+            if (i == nums.length||nums[i] != i) {
+                return i;
+            }
+            
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = new int[]{0,1,2,4};
+        System.out.println(missingNumber(nums));
+    }
+    
+}
