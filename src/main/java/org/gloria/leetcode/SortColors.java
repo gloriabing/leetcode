e4f0b03f5a896d6a8667b2c42e83d9b620b66860
Gloria <zhangbing1990@126.com>
2017-02-23 10:23:36 +0800

    Sort Colors

---
+++ src/main/java/org/gloria/leetcode/SortColors.java
@@ -0,0 +1,16 @@
+package org.gloria.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Create on 2017/2/23 10:22.
+ *
+ * @author : gloria.
+ * 
+ * @see : https://leetcode.com/problems/sort-colors/
+ */ 
+public class SortColors {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
